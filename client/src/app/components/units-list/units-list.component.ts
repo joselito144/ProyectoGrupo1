eeb3bc8d22e02b6391a179d14669a75a32beb60e
Jose Andres Suarez Espinal <jose.suarez@sophossolutions.com>
2019-11-16 10:31:52 -0500

    Cambios Rutas API (De Locales a AWS)

--- client/src/app/components/units-list/units-list.component.ts
+++ client/src/app/components/units-list/units-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { UnitsService} from '../../services/units.service';
-import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-units-list',
@@ -9,26 +9,24 @@ import { Router } from '@angular/router';
 })
 export class UnitsListComponent implements OnInit {
 
+  @HostBinding('class') classes = 'row';
   
   units: any = [];
 
 
-  constructor(private unitService: UnitsService, private router: Router) { }
+  constructor(private unitService: UnitsService) { }
 
   ngOnInit() {
     this.unitService.getUnits().subscribe(
       res => {
-        this.units = res[0];
+        console.log(res);
+        this.units = res.Items;
       },
       err => console.log(err)
 
     );
   }
 
-  goToDetails() {
-    console.log('Lo estoy intentando');
-    this.router.navigate(["details"]);
-  }
 
 
 
