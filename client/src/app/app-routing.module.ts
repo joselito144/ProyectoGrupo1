eb17d84330d681734a7ef0c9b34457dee5b4c88a
Jose Andres Suarez Espinal <jose.suarez@sophossolutions.com>
2019-11-18 19:28:24 -0500

    Boton login

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UnitsListComponent} from '../app/components/units-list/units-list.component';
 import { DatailsComponent} from '../app/components/datails/datails.component';
-import {UnitsFormComponent } from '../app/components/units-form/units-form.component'
+import {UnitsFormComponent } from '../app/components/units-form/units-form.component';
+import { LoginComponent } from '../app/components/login/login.component'
 
 
 const routes: Routes = [
