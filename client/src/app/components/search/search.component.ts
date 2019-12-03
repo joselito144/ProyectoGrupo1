74c19aa960f87ab0db2aa78272f8a154a0e04732
Jose Andres Suarez Espinal <jose.suarez@sophossolutions.com>
2019-12-03 16:08:32 -0500

    detalles busqueda

--- client/src/app/components/search/search.component.ts
+++ client/src/app/components/search/search.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
+  private mensaje: string;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  private searchUnits(valueFrom, valueUntil, rooms, baths, parking) {
+    console.log(valueFrom.value);
+    console.log(valueUntil.value);
+    console.log(rooms.value);
+    console.log(baths.value);
+    console.log(parking.value);
+    if ( valueFrom.value === '' || valueUntil.value === '' || rooms.value === null || baths.value === null || parking.value === null) {
+      this.mensaje = 'Debe diligenciar todos los campos';
+    } else {
+      if ( valueFrom.value > valueUntil.value) {
+        this.mensaje  = 'El valor de arriendo desde debe ser menor que el hasta';
+      } else {
+        this.mensaje = 'Buscando Resultados';
+      }
+    }
   }
 
 }
